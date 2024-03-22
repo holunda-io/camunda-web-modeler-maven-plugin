4e67e6cbccd10b6989dafe8a932ddf0da452e89a
Michael von Bargen <michael.vonbargen@holisticon.de>
2024-03-22 18:54:29 +0100

    chore: refactor into smaller methods

--- src/main/java/BpmDownloadPlugin.java
+++ src/main/java/BpmDownloadPlugin.java
@@ -36,69 +36,80 @@ public class BpmDownloadPlugin extends AbstractMojo {
     @Parameter(property = "documents", readonly = true, required = true)
     private List<ModelerDocument> documents;
 
-    @Override
-    public void execute() {
-        getLog().info("Starting to download files");
+    private final CamundaWebModelerClient client;
 
-        if (path == null) {
-            path = project.getBasedir() + "/src/main/resources";
-        }
-
-        getLog().info("Files will be stored under " + path);
-
-        final CamundaWebModelerClient client = CamundaWebModelerClientBuilder.builder()
+    public BpmDownloadPlugin() {
+        this.client = CamundaWebModelerClientBuilder.builder()
                 .clientId(clientId)
                 .clientSecret(clientSecret)
                 .build();
+        ;
+    }
+
+
+    @Override
+    public void execute() {
+        getLog().info("Starting to download files");
+        if (path == null) {
+            path = project.getBasedir() + "/src/main/resources";
+        }
+        getLog().info("Files will be stored under " + path);
 
 
         documents.forEach(it -> {
-            PubSearchDtoFileMetadataDto fileSearchDto = new PubSearchDtoFileMetadataDto();
-            FileMetadataDto fileMetadataSearch = new FileMetadataDto();
-            fileMetadataSearch.setName(it.name);
-            fileSearchDto.setFilter(fileMetadataSearch);
-            PubSearchResultDtoFileMetadataDto fileSearchResponse = client.searchFiles(fileSearchDto);
-            Optional<String> fileId = Optional.ofNullable(fileSearchResponse.getItems()).map(
-                    items -> items.stream().findFirst().map(FileMetadataDto::getId)).flatMap(file -> file);
-
-
+            Optional<String> fileId = getFileId(it.name);
             fileId.ifPresentOrElse(
                     id -> {
                         DownloadFile downloadFile = Optional.ofNullable(it.mileStone).map(
-                                milestone -> {
-                                    PubSearchDtoMilestoneMetadataDto pubSearchDtoMilestoneMetadataDto = new PubSearchDtoMilestoneMetadataDto();
-                                    MilestoneMetadataDto mileStoneSearch = new MilestoneMetadataDto();
-                                    mileStoneSearch.setName(milestone);
-                                    mileStoneSearch.setFileId(id);
-                                    pubSearchDtoMilestoneMetadataDto.setFilter(mileStoneSearch);
-                                    String mileStoneId = Optional.ofNullable(
-                                            client.searchMilestones(
-                                                            pubSearchDtoMilestoneMetadataDto)
-                                                    .getItems()).map(res -> res.stream().findFirst()).flatMap(
-                                            ms -> ms).map(
-                                            MilestoneMetadataDto::getId).orElseThrow();
-
-                                    MilestoneDto milestone1 = client.getMilestone(UUID.fromString(mileStoneId));
-                                    FileDto dto = client.getFile(UUID.fromString(id));
-                                    return new DownloadFile(dto.getMetadata().getSimplePath(),
-                                            milestone1.getContent());
-                                }
+                                milestone -> getMilestoneFile(id, milestone)
                         ).orElseGet(() -> {
                             FileDto dto = client.getFile(UUID.fromString(id));
                             return new DownloadFile(dto.getMetadata().getSimplePath(), dto.getContent());
                         });
-                        try {
-                            Files.writeString(Path.of(path + "/" + downloadFile.name), downloadFile.content);
-                            getLog().info("Created file " + downloadFile.name);
-                        } catch (IOException e) {
-                            getLog().warn("Could not save file for " + it.name);
-                            throw new RuntimeException(e);
-                        }
+                        downloadFile(downloadFile);
                     },
                     () -> getLog().warn("Could not find file for " + it.name)
             );
         });
+    }
 
+    private Optional<String> getFileId(String fileName) {
+        PubSearchDtoFileMetadataDto fileSearchDto = new PubSearchDtoFileMetadataDto();
+        FileMetadataDto fileMetadataSearch = new FileMetadataDto();
+        fileMetadataSearch.setName(fileName);
+        fileSearchDto.setFilter(fileMetadataSearch);
+        PubSearchResultDtoFileMetadataDto fileSearchResponse = client.searchFiles(fileSearchDto);
+        return Optional.ofNullable(fileSearchResponse.getItems()).map(
+                items -> items.stream().findFirst().map(FileMetadataDto::getId)).flatMap(file -> file);
+    }
+
+    private DownloadFile getMilestoneFile(String fileId, String milestone) {
+        PubSearchDtoMilestoneMetadataDto pubSearchDtoMilestoneMetadataDto = new PubSearchDtoMilestoneMetadataDto();
+        MilestoneMetadataDto mileStoneSearch = new MilestoneMetadataDto();
+        mileStoneSearch.setName(milestone);
+        mileStoneSearch.setFileId(fileId);
+        pubSearchDtoMilestoneMetadataDto.setFilter(mileStoneSearch);
+        String mileStoneId = Optional.ofNullable(
+                client.searchMilestones(
+                                pubSearchDtoMilestoneMetadataDto)
+                        .getItems()).map(res -> res.stream().findFirst()).flatMap(
+                ms -> ms).map(
+                MilestoneMetadataDto::getId).orElseThrow();
+
+        MilestoneDto milestone1 = client.getMilestone(UUID.fromString(mileStoneId));
+        FileDto dto = client.getFile(UUID.fromString(fileId));
+        return new DownloadFile(dto.getMetadata().getSimplePath(),
+                milestone1.getContent());
+    }
+
+    private void downloadFile(DownloadFile file) {
+        try {
+            Files.writeString(Path.of(path + "/" + file.name), file.content);
+            getLog().info("Created file " + file.name);
+        } catch (IOException e) {
+            getLog().warn("Could not save file for " + file.name);
+            throw new RuntimeException(e);
+        }
     }
 }
 
