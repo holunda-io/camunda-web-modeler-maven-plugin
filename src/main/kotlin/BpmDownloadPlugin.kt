7467c8aa06343fc032ecc9aeaafa910ad4ea7c36
Christian Maschmann <christian.maschmann@holisticon.de>
2024-04-29 15:53:34 +0200

    feat: create target folder if needed

--- src/main/kotlin/BpmDownloadPlugin.kt
+++ src/main/kotlin/BpmDownloadPlugin.kt
@@ -1,3 +1,4 @@
+
 import io.holunda.webmodeler.rest.CamundaWebModelerClientBuilder
 import io.holunda.webmodeler.rest.impl.CamundaWebModelerClient
 import org.apache.maven.plugin.AbstractMojo
@@ -5,19 +6,11 @@ import org.apache.maven.plugins.annotations.LifecyclePhase
 import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
 import org.apache.maven.project.MavenProject
-import org.openapitools.client.model.FileDto
-import org.openapitools.client.model.FileMetadataDto
-import org.openapitools.client.model.MilestoneDto
-import org.openapitools.client.model.MilestoneMetadataDto
-import org.openapitools.client.model.PubSearchDtoFileMetadataDto
-import org.openapitools.client.model.PubSearchDtoMilestoneMetadataDto
-import org.openapitools.client.model.PubSearchResultDtoFileMetadataDto
-import java.io.IOException
+import org.openapitools.client.model.*
 import java.nio.file.Files
 import java.nio.file.Path
-import java.util.Objects
-import java.util.Optional
-import java.util.UUID
+import java.util.*
+import kotlin.io.path.createDirectories
 
 @Mojo(name = "bpmn-download", defaultPhase = LifecyclePhase.COMPILE)
 class BpmDownloadPlugin: AbstractMojo() {
@@ -45,11 +38,11 @@ class BpmDownloadPlugin: AbstractMojo() {
             .build()
 
 
-        getLog().info("Starting to download files")
+        log.info("Starting to download files")
         if (path == null) {
             path = project.basedir.toPath().resolve("/src/main/resources").toString()
         }
-        getLog().info("Files will be stored under $path")
+        log.info("Files will be stored under $path")
 
 
         documents.forEach { it ->
@@ -60,61 +53,62 @@ class BpmDownloadPlugin: AbstractMojo() {
                         Optional.ofNullable(it.mileStone).map { milestone -> getMilestoneFile(id, milestone) }
                             .orElseGet {
                                 val dto: FileDto = client!!.getFile(UUID.fromString(id))
-                                Objects.requireNonNull(dto.getMetadata(), "metadata of file '$id' is null")
-                                DownloadFile(dto.getMetadata().getSimplePath(), dto.getContent())
+                                Objects.requireNonNull(dto.metadata, "metadata of file '$id' is null")
+                                DownloadFile(dto.metadata.simplePath, dto.content)
                             }
                     downloadFile(downloadFile)
                 },
-                { getLog().warn("Could not find file for " + it.name) }
+                { log.warn("Could not find file for " + it.name) }
             )
         }
     }
 
     private fun getFileId(fileName: String): Optional<String> {
         val fileMetadataSearch = FileMetadataDto()
-        fileMetadataSearch.setName(fileName)
+        fileMetadataSearch.name = fileName
         val fileSearchDto = PubSearchDtoFileMetadataDto()
-        fileSearchDto.setFilter(fileMetadataSearch)
+        fileSearchDto.filter = fileMetadataSearch
 
         val fileSearchResponse: PubSearchResultDtoFileMetadataDto = client!!.searchFiles(fileSearchDto)
         if (fileSearchResponse.total != 1) {
             throw IllegalStateException("file search for $fileMetadataSearch returned ${fileSearchResponse.total} results, should be one")
         }
-        return Optional.ofNullable(fileSearchResponse.getItems())
+        return Optional.ofNullable(fileSearchResponse.items)
             .map { items -> items.stream().findFirst().map(FileMetadataDto::getId) }
             .flatMap { file -> file }
     }
 
     private fun getMilestoneFile(fileId: String, milestoneName: String): DownloadFile {
         val mileStoneSearch = MilestoneMetadataDto()
-        mileStoneSearch.setName(milestoneName)
-        mileStoneSearch.setFileId(fileId)
+        mileStoneSearch.name = milestoneName
+        mileStoneSearch.fileId = fileId
 
         val pubSearchDtoMilestoneMetadataDto = PubSearchDtoMilestoneMetadataDto()
-        pubSearchDtoMilestoneMetadataDto.setFilter(mileStoneSearch)
+        pubSearchDtoMilestoneMetadataDto.filter = mileStoneSearch
 
         val mileStoneId: String = Optional.ofNullable(
             client!!.searchMilestones(
                 pubSearchDtoMilestoneMetadataDto
             )
-                .getItems()
+                .items
         ).map { res -> res.stream().findFirst() }.flatMap { ms -> ms }.map(
             MilestoneMetadataDto::getId
         ).orElseThrow()
 
         val milestone: MilestoneDto = client!!.getMilestone(UUID.fromString(mileStoneId))
         val dto: FileDto = client!!.getFile(UUID.fromString(fileId))
-        Objects.requireNonNull(dto.getMetadata(), "metadata of file '$fileId' and milestone '$milestoneName' is null")
+        Objects.requireNonNull(dto.metadata, "metadata of file '$fileId' and milestone '$milestoneName' is null")
         return DownloadFile(
-            dto.getMetadata().getSimplePath(),
-            milestone.getContent()
+            dto.metadata.simplePath,
+            milestone.content
         )
     }
 
     private fun downloadFile(file: DownloadFile) {
         path?.let {
+            Path.of(it).createDirectories()
             Files.writeString(Path.of(it, file.name), file.content)
-            getLog().info("Created file " + file.name)
+            log.info("Created file " + file.name)
         } ?: throw IllegalArgumentException("path not set in plugin configuration")
     }
 }
